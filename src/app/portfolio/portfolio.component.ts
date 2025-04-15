6e3b690cf00b5f204628aab0183f8124668c5f82
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2025-04-15 10:29:51 +0200

    insert new imgs for project el_pollo_loco

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -75,8 +75,8 @@ export class PortfolioComponent {
       technologies: ['JavaScript', 'HTML', 'CSS'],
       githubLink: 'https://github.com/PhilippSchoenborn/El_Pollo_Loco_Phil_Cenk',
       liveLink: 'https://philipp-schoenborn.de/elpolloloco',
-      imageUrl: 'assets/img/pop-ups/el_pollo_loco_pop_up.svg',
-      modalImageUrl: 'assets/img/pop-ups/modal_loco.svg',
+      imageUrl: 'assets/img/pop-ups/el_pollo_loco.png',
+      modalImageUrl: 'assets/img/pop-ups/modal_loco.png',
     },
     // {
     //   id: 3,
