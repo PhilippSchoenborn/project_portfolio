f8e0b94a1d190a4a39282778db638a32597a0fd9
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2025-04-17 13:41:58 +0200

    bug fixing - fixed folder stucture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes }             from '@angular/router';
-import { HomeComponent }      from './app/home/home.component';
+import { HomeComponent }      from './home/home.component';
 import { LegalNoticeComponent } from './footer/legal-notice/legal-notice.component';
 
 export const routes: Routes = [
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component }      from '@angular/core';
 import { CommonModule }   from '@angular/common';
-import { HeroComponent }      from '../../hero/hero.component';
-import { AboutComponent }     from '../../about/about.component';
-import { SkillsComponent }    from '../../skills/skills.component';
-import { PortfolioComponent } from '../../portfolio/portfolio.component';
-import { CommentsComponent }  from '../../comments/comments.component';
-import { ContactComponent }   from '../../contact/contact.component';
-import { FooterComponent }    from '../../footer/footer.component';
+import { HeroComponent }      from '../hero/hero.component';
+import { AboutComponent }     from '../about/about.component';
+import { SkillsComponent }    from '../skills/skills.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
+import { CommentsComponent }  from '../comments/comments.component';
+import { ContactComponent }   from '../contact/contact.component';
+import { FooterComponent }    from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
