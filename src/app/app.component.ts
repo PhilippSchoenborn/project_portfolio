87ed7e5ac1330a4272958bad6a26c391c61dd07e
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2025-04-22 09:26:29 +0200

    clean code / JSdocs for app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-// src/app/app.component.ts
 import { Component }      from '@angular/core';
 import { RouterOutlet }   from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
+/**
+ * The root component of the application.
+ * This component serves as the wrapper for the entire app, containing the header, footer, 
+ * and the router outlet where different views (components) will be rendered based on the active route.
+ */
 @Component({
   selector: 'app-root',
   standalone: true,
