cc0a328783938ea6f506d94e9e1d07caffcdea83
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2025-04-22 09:00:28 +0200

    clean code / JSdocs for contact.component.spec.ts

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -1,22 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ContactComponent } from './contact.component';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
 
+  /**
+   * Asynchronous setup before each test case.
+   * Configures the testing module and creates the component fixture.
+   */
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ContactComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ContactComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  /**
+   * Unit test to verify that the component is successfully created.
+   */
   it('should create', () => {
     expect(component).toBeTruthy();
   });
